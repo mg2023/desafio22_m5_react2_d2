f8bc4127c4d4f97aa6f8c7c59643a6532fb6db74
mg <amg.vergara@gmail.com>
2022-12-22 23:08:10 -0300

    Se pueden mostrar las fotos y marcar los corazones

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./styles.css";
+import MyContext from "./my_context";
+import { useEffect, useState } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -8,17 +10,39 @@ import Favoritos from "./views/Favoritos";
 
 export default function App() {
   const endpoint = "/fotos.json";
+  const [fotos, setFotos] = useState([])
+  const sharedState = { fotos, setFotos }
+
+  const getFotos = async () => {
+    const response = await fetch(endpoint)
+    let { photos } = await response.json()
+    photos = photos.map((photo) => ({
+      id: photo.id,
+      src: photo.src.tiny,
+      desc: photo.alt,
+      favorito: false
+    }))
+    setFotos(photos)
+
+  }
+
+  useEffect(() => {
+    getFotos()
+  }, [])
+
+  console.log(fotos)
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favoritos" element={<Favoritos />} />
-        </Routes>
-      </BrowserRouter>
+      <MyContext.Provider value={sharedState}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favoritos" element={<Favoritos />} />
+          </Routes>
+        </BrowserRouter>
+      </MyContext.Provider>
     </div>
   );
 }
--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -1,11 +1,33 @@
 import "../assets/css/galeria.css";
+import { useContext } from 'react';
 import Heart from "./Heart";
+import MyContext from "../my_context";
 
-export default function Home() {
+export default function Galeria() {
+  const { fotos, setFotos } = useContext(MyContext)
+  // console.log(fotos)
+
+  const setFavorito = (id) => {
+    const fotoIndex = fotos.findIndex((f) => f.id === id)
+    fotos[fotoIndex].favorito = !fotos[fotoIndex].favorito
+    setFotos([...fotos])
+  }
 
   return (
     <div className="galeria grid-columns-5 p-3">
-
+      {
+        fotos.map( (foto, i) => (
+          <div 
+          onClick={() => setFavorito(foto.id)}
+          className="foto"
+          style={{ backgroundImage: `url(${foto.src})` }}
+          key={i}
+          >
+            <Heart filled={foto.favorito} />
+            <p> {foto.desc}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
