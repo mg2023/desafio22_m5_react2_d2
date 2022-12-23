025df678fca734d7080b165278dca1a7ec54edc9
mg <amg.vergara@gmail.com>
2022-12-22 23:34:31 -0300

    Se renderizan las fotos favoritas

--- src/views/Favoritos.js
+++ src/views/Favoritos.js
@@ -1,10 +1,25 @@
+import { useContext } from "react"
+import MyContext from "../my_context";
+
 export default function Favoritos() {
-  
+
+  const { fotos, setFotos } = useContext(MyContext)
+  const num_fotos_favoritas = (fotos.filter((foto) => foto.favorito)).length
+
   return (
     <div>
       <h1>Fotos favoritas</h1>
       <div className="p-3 galeria grid-columns-4">
-       
+        {
+          num_fotos_favoritas == 0 ?
+            <p>No hay fotos favoritas</p> :
+            fotos
+              .filter((foto) => foto.favorito)
+              .map((foto, i) => (
+                <img src={foto.src} alt={i} />
+              )
+              )
+        }
       </div>
     </div>
   );
